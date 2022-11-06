d9de6e5665558d2e023f6e6f675e1a47522d3f36
Revangerrrr <92561581+Revangerrrr@users.noreply.github.com>
2022-11-07 00:06:38 +0700

    Add files via upload

---
+++ Lab3_3/src/main/java/com/example/lab3_3/HelloController.java
@@ -0,0 +1,70 @@
+package com.example.lab3_3;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+
+public class HelloController {
+    @FXML
+public void initialize() {
+    pauseButton.setDisable(true);
+    stopButton.setDisable(true);
+}
+
+    @FXML
+    private ProgressBar progress;
+    @FXML
+    private Button pauseButton;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button stopButton;
+    private boolean running = false;
+    Model model;
+    @FXML
+    protected void startClicked()
+    {
+        if (!running) {
+            startButton.setText("Перезапуск");
+            model = new Model(progress);
+            running = true;
+            pauseButton.setDisable(false);
+            stopButton.setDisable(false);
+        }
+
+        else
+            model.barThread.setRerun(true);
+    }
+    @FXML
+    protected void stopClicked()
+    {
+        startButton.setText("Старт");
+        model.barThread.interrupt();
+        progress.setProgress(0);
+        running = false;
+        pauseButton.setDisable(true);
+        stopButton.setDisable(true);
+    }
+    @FXML
+    protected void pauseClicked()
+    {
+        if (model.barThread.getIsPaused()) {
+            model.barThread.setIsPaused(false);
+            pauseButton.setText("Пауза");
+            synchronized (System.out) {
+                System.out.notify();
+            }
+            stopButton.setDisable(false);
+            startButton.setDisable(false);
+
+        }
+        else {
+            model.barThread.setIsPaused(true);
+            pauseButton.setText("Продолжить");
+            startButton.setDisable(true);
+            stopButton.setDisable(true);
+        }
+    }
+}
---
+++ Lab3_3/src/main/java/com/example/lab3_3/Model.java
@@ -0,0 +1,71 @@
+package com.example.lab3_3;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+public class Model {
+    public boolean running;
+    public progressBarThread barThread;
+
+    private ProgressBar progress;
+
+    Model(ProgressBar progress) {
+        this.progress = progress;
+        barThread = new progressBarThread();
+        barThread.setUpdater(new Updatable() {
+            public void update(double value) {
+                Platform.runLater(new Runnable() {
+                    public void run() {
+                        progress.setProgress(value);
+                    }
+                });
+            }
+        });
+        barThread.start();
+        running = true;
+    }
+}
+interface  Updatable{
+    public void update(double value);
+}
+
+class progressBarThread extends Thread {
+    Updatable updater;
+    public void setUpdater(Updatable updater) {this.updater = updater;}
+    private boolean isPaused = false;
+    public boolean getIsPaused() { return isPaused; }
+    public void setIsPaused(boolean isPaused) {  this.isPaused = isPaused; }
+    private boolean rerun = false;
+    public boolean getRerun() { return rerun; }
+    public void setRerun(boolean rerun) { this.rerun = rerun;}
+    private double percents = 0;
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 1000; i++) {
+                if(rerun) {
+                    i = 0;
+                    rerun = false;
+                }
+                percents = i * 0.001;
+
+                updater.update(percents);
+                Thread.sleep(20);
+                if(isPaused) {
+                    try {
+                        synchronized (System.out) {
+                            System.out.wait();
+                        }
+                    } catch (InterruptedException e) {
+                        this.interrupt();
+                    }
+                }
+            }
+
+        }
+        catch (InterruptedException e) {
+            this.interrupt();
+        }
+    }
+}
